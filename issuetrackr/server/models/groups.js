4c061f490d33c588ba24fd300bb7a108831d3c3b
ㅎㅌㅎ <ds3mbc.dev@gmail.com>
2014-05-04 23:30:07 +0900

    Insert comments

--- issuetrackr/server/models/groups.js
+++ issuetrackr/server/models/groups.js
@@ -7,26 +7,32 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var GroupsSchema = new Schema({
+	//기사 
 	article: [{
+		// 기사 제목 
 		title: { 
 			type: String,
 			required: true,
 		},
+		// 기사 이미지 
 		img: {
 			type: String,
 		},
+		// 기사 링크 
 		link: {
 			type: String,
 			required: true,
 		},
+		//기사 작성 일자 
 		date: {
 			type: String,
 			required: true,
 		},
+		//기사 올린 이 
 		user: {
         type: Schema.ObjectId,
         ref: 'User'
-    }
+    	}
 	}]
 });
 
--- issuetrackr/server/models/issue.js
+++ issuetrackr/server/models/issue.js
@@ -8,49 +8,63 @@ var mongoose = require('mongoose'),
 
 
 /**
- * Article Schema
+ * Issue Schema
  */
 var IssueSchema = new Schema({
+	//태그
 	tag:{
 		type:[String],
 		required: true
 		},
+	//이슈 제목 
 	title:{
 		type: String,
 		required: true
 		trim: true
 		},
+	//이슈가 시작된 날짜
     start:{
 		type: Date,
 		default: Date.now
 		},
+	//이슈에 포함된 그룹 
     Groups: {
 		type:[{
 		type:Schema.ObjectId,
 		ref:"Groups"}],
 			default:[]
 		},
-	isClosed:{type:Boolean,
+	//닫힌 이슈인지 
+	isClosed:{
+		type:Boolean,
 		default:false
 		},
-	lastUpdate: {type:Date,
-				default: Date.now
+	//마지막으로 업데이트된 날짜 
+	lastUpdate: {
+		type:Date,
+		default: Date.now
 		},
-	writer:{type:[{
+	//작성한 사람 
+	writer:{
+		type:[{
 		type:Schema.ObjectId,
 		ref:"User"}],
 			default:[]
 		},
-	commentator:{type:[{
+	//댓글 단 사람 
+	commentator:{
+		type:[{
 		type:Schema.ObjectId,
 		ref:"User"}],
 			default:[]
 		},
-	follower:{type:[{
+	//팔로우하는 사람
+	follower:{
+		type:[{
 		type:Schema.ObjectId,
 		ref:"User"}],
 			default:[]
-		},
+		}
 });
 
 mongoose.model('Issue', IssueSchema);
