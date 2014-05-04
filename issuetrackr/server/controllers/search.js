fbcd247cf6be00b42a3bbb3b28ac83829380d9b1
ㅎㅌㅎ <ds3mbc.dev@gmail.com>
2014-05-05 00:44:32 +0900

    Insert comments && Reordering(issue.js)

--- issuetrackr/server/controllers/search.js
+++ issuetrackr/server/controllers/search.js
@@ -11,13 +11,16 @@ var mongoose = require('mongoose'),
  * Find Articles
  */
 exports.search = function(req, res) {
+    //쿼리를 생성합니다. 
     var query = {};
 
+    //정렬순서가 없을 시, 에러를 내놓습니다.
     if (!req.query.sortorder){
         res.render('error', {status: 500});
         return;
     }
 
+    //검색 키워드가 없을 시, 추가창을 만듭니다 - 수정좀 
     if (!req.query.keyword){
         query = {
             '$or':[{
@@ -30,10 +33,12 @@ exports.search = function(req, res) {
         };
     }
 
+    //카테고리를 받았으면, 쿼리에 내용을 추가합니다.
     if (req.query.category){
         query.category = req.query.category;
     }
 
+    //쿼리를 이용해 DB에서 검색합니다.
     Issue.find(query).sort(req.query.sortorder).exec(function(err, issue) {
         if (err) {
             res.render('error', {status: 500});
