24b95604557408c9f68aa5290074d1f1c2646494
jungbin12345 <jungbin12345@naver.com>
2014-05-05 22:13:26 +0900

    마무리같아요!! ㅎㅎㅎㅎㅎ ^^

--- issuetrackr/public/index/controllers/index.js
+++ issuetrackr/public/index/controllers/index.js
@@ -2,4 +2,15 @@
 
 angular.module('mean.index').controller('MainController', ['$scope', 'Global', function ($scope, Global) {
     $scope.global = Global;
+	$scope.categories=["정치", "문화", "사회", "경제", "교육", "인물"];
+	$scope.home=function(){
+		location.reload();
+	};
+	$scope.login=function(){
+		location.href="/signin";
+	};
+	$scope.logout=function(){
+		location.href="/signout";
+	};
 }]);
+ 
